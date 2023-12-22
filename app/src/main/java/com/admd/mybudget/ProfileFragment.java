05dc98fff02dd452e62da03216a006e2e5de5d56
Intel House <chathurashamikaindrguptha@gmail.com>
2023-12-22 10:47:54 +0530

    2023/12/20-Commit

--- app/src/main/java/com/admd/mybudget/ProfileFragment.java
+++ app/src/main/java/com/admd/mybudget/ProfileFragment.java
@@ -4,60 +4,66 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class ProfileFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private FirebaseFirestore firestore;
+    private FirebaseAuth mAuth;
+    private EditText editName;
+    private EditText editMnumber;
+    private EditText editEmail;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        firestore = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        editName = view.findViewById(R.id.editName);
+        editMnumber = view.findViewById(R.id.editMnumber);
+        editEmail = view.findViewById(R.id.editEmail);
+
+        fetchData();
+
+        return view;
+    }
+
+    private void fetchData() {
+        if (mAuth.getCurrentUser() != null) {
+            String userID = mAuth.getCurrentUser().getUid();
+
+            firestore.collection("users").document(userID).get().addOnCompleteListener(profileTask -> {
+                if (profileTask.isSuccessful()) {
+                    DocumentSnapshot documentSnapshot = profileTask.getResult();
+                    if (documentSnapshot != null && documentSnapshot.exists()) {
+
+                        String userName = documentSnapshot.getString("mName");
+                        String mobileNumber = documentSnapshot.getString("mMobilenumber");
+                        String email = documentSnapshot.getString("mEmail");
+
+                        // Check for null values before setting to EditText
+                        if (userName != null) {
+                            editName.setText(userName);
+                        }
+
+                        if (mobileNumber != null) {
+                            editMnumber.setText(mobileNumber);
+                        }
+
+                        if (email != null) {
+                            editEmail.setText(email);
+                        }
+                    }
+                }
+            });
         }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
